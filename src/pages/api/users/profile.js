f8f7fa9b7fc2f23daf1c2a9ab5148490bb36fd03
yogawan <yogawanadityapratama@gmail.com>
2025-03-03 11:49:46 +0700

    Memperbaiki erorr saat deploy ke vercel

--- src/pages/api/users/profile.js
+++ src/pages/api/users/profile.js
@@ -1,17 +1,17 @@
-import connectDB from "../../../lib/mongodb";
-import User from "../../../models/userModel";
-import checkAuth from "../middleware/auth";
+// import connectDB from "../../../lib/mongodb";
+// import User from "../../../models/userModel";
+// import checkAuth from "../middleware/auth";
 
-const handler = async (req, res) => {
-  await connectDB();
+// const handler = async (req, res) => {
+//   await connectDB();
 
-  const user = await User.findById(req.user.userId).select("-password");
+//   const user = await User.findById(req.user.userId).select("-password");
 
-  if (!user) {
-    return res.status(404).json({ success: false, message: "User tidak ditemukan" });
-  }
+//   if (!user) {
+//     return res.status(404).json({ success: false, message: "User tidak ditemukan" });
+//   }
 
-  return res.status(200).json({ success: true, user });
-};
+//   return res.status(200).json({ success: true, user });
+// };
 
-export default checkAuth(handler);
+// export default checkAuth(handler);
